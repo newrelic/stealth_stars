bb25cfbb62a94ed1027d9220db6a6568a6fa8a05
Ben Weintraub <ben@newrelic.com>
2013-04-23 21:05:44 -0700

    Send unicorn output to log files

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -5,16 +5,15 @@
 # more will usually help for _short_ waits on databases/caches.
 worker_processes 3
 
+stderr_path "log/unicorn.stderr.log"
+stdout_path "log/unicorn.stdout.log"
+
 # Since Unicorn is never exposed to outside clients, it does not need to
 # run on the standard HTTP port (80), there is no reason to start Unicorn
 # as root unless it's from system init scripts.
 # If running the master process as root and the workers as an unprivileged
 # user, do this to switch euid/egid in the workers (also chowns logs):
 # user "unprivileged_user", "unprivileged_group"
-
-# Help ensure your application will always spawn in the symlinked
-# "current" directory that Capistrano sets up.
-#working_directory "//current" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
