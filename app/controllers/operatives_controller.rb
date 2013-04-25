285f10235fd6ba11849b92e9909543bbfae43e79
Ben Weintraub <ben@newrelic.com>
2013-04-25 13:35:00 -0700

    Tweak params for traffic generation script to work better for demo

--- app/controllers/operatives_controller.rb
+++ app/controllers/operatives_controller.rb
@@ -1,6 +1,6 @@
 class OperativesController < ApplicationController
   def index
-    @operatives = Operative.includes(:missions).all
+    @operatives = Operative.includes(:missions).limit(50).all
 
     respond_to do |format|
       format.json { render :json => @operatives.to_json(:include => :missions ) }
